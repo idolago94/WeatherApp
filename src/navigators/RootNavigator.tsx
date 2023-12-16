3c229f103fb31786186c931549ec12ab2e56a0c9
Ido Lago <idolago94@gmail.com>
2023-12-16 22:22:59 +0200

    header strings

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { CitiesListScreen, CityDetailsScreen } from "@screens";
 import { ScreenNames } from "@constants";
+import { useStrings } from "@hooks";
 
 const Stack = createNativeStackNavigator();
 
 export const RootNavigator: React.FC = () => {
+    const strings = useStrings()
 
     return (
         <Stack.Navigator initialRouteName={ScreenNames.CITIES}>
-            <Stack.Screen name={ScreenNames.CITIES} component={CitiesListScreen} />
-            <Stack.Screen name={ScreenNames.CITY_DETAILS} component={CityDetailsScreen} />
+            <Stack.Screen
+                name={ScreenNames.CITIES}
+                component={CitiesListScreen}
+                options={{ title: strings.cities_header_screen }}
+            />
+            <Stack.Screen
+                name={ScreenNames.CITY_DETAILS}
+                component={CityDetailsScreen}
+                options={{ title: strings.city_details_header_screen }}
+            />
         </Stack.Navigator>
     )
 }
